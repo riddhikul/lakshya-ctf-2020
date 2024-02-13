91b0047eddcbaa74aa26b9880f41d7982390c4ed
prasad <hackerspider09@gmail.com>
2024-02-14 00:23:35 +0530

    after deployement 404 error

--- CTFFinal/settings-azure.py
+++ CTFFinal/settings-azure.py
@@ -6,6 +6,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+print(CSRF_TRUSTED_ORIGINS)
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
