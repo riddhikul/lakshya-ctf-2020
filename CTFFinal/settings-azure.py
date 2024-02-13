8cce6723459f6d0b2b26730f9ff49d1bd64a6fda
prasad <hackerspider09@gmail.com>
2024-02-14 00:46:49 +0530

    add debug

--- CTFFinal/settings-azure.py
+++ CTFFinal/settings-azure.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]+[os.environ['CUSTOME_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 print(CSRF_TRUSTED_ORIGINS)
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
