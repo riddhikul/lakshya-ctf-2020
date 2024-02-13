3fcba759ea95e1db1a8ca405cc30c73bdd2ff49c
prasad <hackerspider09@gmail.com>
2024-02-13 23:33:01 +0530

    cache removed

--- CTFFinal/settings-azure.py
+++ CTFFinal/settings-azure.py
@@ -44,16 +44,16 @@ DATABASES = {
 }
 
 
-CACHES = {
-        "default": {  
-            "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": os.environ.get('AZURE_REDIS_CONNECTIONSTRING'),
-            "OPTIONS": {
-                "CLIENT_CLASS": "django_redis.client.DefaultClient",
-                "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
-        },
-    }
-}
+# CACHES = {
+#         "default": {  
+#             "BACKEND": "django_redis.cache.RedisCache",
+#             "LOCATION": os.environ.get('AZURE_REDIS_CONNECTIONSTRING'),
+#             "OPTIONS": {
+#                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#                 "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
+#         },
+#     }
+# }
 
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
